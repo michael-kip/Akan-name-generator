b335e25fda0de8b71279403842e97c72b793253c
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2021-11-16 16:10:56 +0300

    updating the margin for legend and and gender and date in js if statement

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-let namesOfFemales = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-let namesOfMales = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-let daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
-function akanGenerator (event){
+function akanGenerator (){
     let date = document.getElementById("date").value;
 
     let gender = document.getElementById("gender").value;
 
     let dayValue = new Date(date).getDay()
-    if(gender==""||date==""){
-        alert("Error! provide date and gender" )
+
+    let namesOfFemales = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+    let namesOfMales = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+    
+    let daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
+
+    if(date==""||gender==""){
+        alert("Error! Invalid date and gender" )
     }
     else if(gender=="female"){
-        alert("You were born on" +  daysOfWeek[dayValue] + "," + "your Akan name is" + ","+ namesOfFemales[dayValue])
-        date.value=""; gender="";
+        alert("You were born on" +  daysOfWeek [dayValue] + "," + "your Akan name is" + ","+ namesOfFemales [dayValue])
+        date.value=""; gender.value="";
     }
     else{
-        alert("You were born on" + daysOfWeek[dayValue] + "," + "your Akan name is" +"" + namesOfMales[dayValue])
+        alert("You were born on" + daysOfWeek [dayValue] + "," + "your Akan name is" +"" + namesOfMales [dayValue])
         date.value=""; gender.value="";
     } 
  
