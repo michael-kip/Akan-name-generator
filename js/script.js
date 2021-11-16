941c45f936c3b1786dda67089c2f123ebaa76855
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2021-11-16 08:33:10 +0300

    updating js file with if and  else statements to check for date, gender, and akan name

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let namesOfFemales = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 let namesOfMales = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
 let daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
-
 function akanGenerator (event){
     event.preventDefault()
     let date = document.getElementById("date").value;
@@ -10,7 +9,16 @@ function akanGenerator (event){
     let gender = document.getElementById("gender").value;
 
     let dayValue = new Date(date).getDay()
-    if(gender=="female"){
+    if(gender==""||date==""){
+        alert("Error! provide date and gender" )
+    }
+    else if(gender=="female"){
         alert("You were born on" + daysOfWeek[dayValue] + "," + "your Akan name is" + namesOfFemales[dayValue])
         gender();
     }
+    else{
+        alert("You were born on" + daysOfWeek[dayValue] + "," + "your Akan name is" + namesOfMales[dayValue])
+    } 
+ 
+}
+
