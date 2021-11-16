7762b6ee4103841c1c874b284cc8695b26ec6683
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2021-11-16 08:00:04 +0300

    adding a function to generate names on clicking button, initializing variables for gender and date inside it

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ let namesOfFemales = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 let namesOfMales = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
 let daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
+
+function akanGenerator (event){
+    event.preventDefault()
+    let date = document.getElementById("date").value;
+
+    let gender = document.getElementById("gender").value;
